10e9d8d1736309548a554bdc3ef837553c411c54
Vicnandez-mar <victormardez@outlook.com>
2026-03-09 01:27:36 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,47 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Au38Sv27yiAJpH4PXop6Mjv4ewn58fdd3ArvymAh4oXL");
+declare_id!("BnfyhYagCQeEiUmkoYvhAnwiGhT7rCHeTwRJnPLUQSYq");
 
 #[program]
-pub mod veterinaria {
+pub mod veterinary {
     use super::*;
 
-    // Crear un nuevo registro
     pub fn create_pet(
         ctx: Context<CreatePet>,
-        nombre: String,
-        raza: String,
-        especie: String,
-        edad: u8,
-        vacunacion: String,
-        dueno: String,
-        enfermedades: String,
+        name: String,
+        breed: String,
+        species: String,
+        age: u8,
+        vaccination: String,
+        owner: String,
+        diseases: String,
     ) -> Result<()> {
-        if nombre.is_empty() || dueno.is_empty() {
+        if name.is_empty() || owner.is_empty() {
             return Err(ErrorCode::InvalidInput.into());
         }
 
         let pet_account = &mut ctx.accounts.pet_account;
-        pet_account.nombre = nombre;
-        pet_account.raza = raza;
-        pet_account.especie = especie;
-        pet_account.edad = edad;
-        pet_account.vacunacion = vacunacion;
-        pet_account.dueno = dueno;
-        pet_account.enfermedades = enfermedades;
+        pet_account.name = name;
+        pet_account.breed = breed;
+        pet_account.species = species;
+        pet_account.age = age;
+        pet_account.vaccination = vaccination;
+        pet_account.owner = owner;
+        pet_account.diseases = diseases;
         pet_account.user = ctx.accounts.user.key();
 
         Ok(())
     }
 
-    // Modificar un registro existente
     pub fn update_pet(
         ctx: Context<UpdatePet>,
-        nombre: String,
-        raza: String,
-        especie: String,
-        edad: u8,
-        vacunacion: String,
-        dueno: String,
-        enfermedades: String,
+        name: String,
+        breed: String,
+        species: String,
+        age: u8,
+        vaccination: String,
+        owner: String,
+        diseases: String,
     ) -> Result<()> {
         let pet_account = &mut ctx.accounts.pet_account;
 
@@ -51,29 +49,29 @@ pub mod veterinaria {
             return Err(ErrorCode::Unauthorized.into());
         }
 
-        pet_account.nombre = nombre;
-        pet_account.raza = raza;
-        pet_account.especie = especie;
-        pet_account.edad = edad;
-        pet_account.vacunacion = vacunacion;
-        pet_account.dueno = dueno;
-        pet_account.enfermedades = enfermedades;
+        pet_account.name = name;
+        pet_account.breed = breed;
+        pet_account.species = species;
+        pet_account.age = age;
+        pet_account.vaccination = vaccination;
+        pet_account.owner = owner;
+        pet_account.diseases = diseases;
+
         Ok(())
     }
 
     pub fn get_pet(ctx: Context<GetPet>) -> Result<()> {
         let pet_account = &ctx.accounts.pet_account;
-        msg!("Mascota: {}", pet_account.nombre);
-        msg!("Raza: {}", pet_account.raza);
-        msg!("Especie: {}", pet_account.especie);
-        msg!("Edad: {}", pet_account.edad);
-        msg!("Vacunación: {}", pet_account.vacunacion);
-        msg!("Dueño: {}", pet_account.dueno);
-        msg!("Enfermedades: {}", pet_account.enfermedades);
+        msg!("Mascota: {}", pet_account.name);
+        msg!("Raza: {}", pet_account.breed);
+        msg!("Especie: {}", pet_account.species);
+        msg!("Edad: {}", pet_account.age);
+        msg!("Vacunación: {}", pet_account.vaccination);
+        msg!("Dueño: {}", pet_account.owner);
+        msg!("Enfermedades: {}", pet_account.diseases);
         Ok(())
     }
 
-    // Borrar un registro
     pub fn delete_pet(ctx: Context<DeletePet>) -> Result<()> {
         let pet_account = &ctx.accounts.pet_account;
         if pet_account.user != ctx.accounts.user.key() {
@@ -85,13 +83,13 @@ pub mod veterinaria {
 
 // Contextos con seeds
 #[derive(Accounts)]
-#[instruction(nombre: String)]
+#[instruction(name: String)]
 pub struct CreatePet<'info> {
     #[account(
         init,
         payer = user,
         space = 8 + PetAccount::MAX_SIZE,
-        seeds = [b"pet", user.key().as_ref(), nombre.as_bytes()],
+        seeds = [b"pet", user.key().as_ref(), name.as_bytes()],
         bump
     )]
     pub pet_account: Account<'info, PetAccount>,
@@ -102,20 +100,20 @@ pub struct CreatePet<'info> {
 
 #[derive(Accounts)]
 pub struct UpdatePet<'info> {
-    #[account(mut, seeds = [b"pet", user.key().as_ref(), pet_account.nombre.as_bytes()], bump)]
+    #[account(mut, seeds = [b"pet", user.key().as_ref(), pet_account.name.as_bytes()], bump)]
     pub pet_account: Account<'info, PetAccount>,
     pub user: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct GetPet<'info> {
-    #[account(seeds = [b"pet", pet_account.user.as_ref(), pet_account.nombre.as_bytes()], bump)]
+    #[account(seeds = [b"pet", pet_account.user.as_ref(), pet_account.name.as_bytes()], bump)]
     pub pet_account: Account<'info, PetAccount>,
 }
 
 #[derive(Accounts)]
 pub struct DeletePet<'info> {
-    #[account(mut, close = user, seeds = [b"pet", user.key().as_ref(), pet_account.nombre.as_bytes()], bump)]
+    #[account(mut, close = user, seeds = [b"pet", user.key().as_ref(), pet_account.name.as_bytes()], bump)]
     pub pet_account: Account<'info, PetAccount>,
     pub user: Signer<'info>,
 }
@@ -123,18 +121,18 @@ pub struct DeletePet<'info> {
 // Datos de la mascota
 #[account]
 pub struct PetAccount {
-    pub nombre: String,
-    pub raza: String,
-    pub especie: String,
-    pub edad: u8,
-    pub vacunacion: String,
-    pub dueno: String,
-    pub enfermedades: String,
+    pub name: String,
+    pub breed: String,
+    pub species: String,
+    pub age: u8,
+    pub vaccination: String,
+    pub owner: String,
+    pub diseases: String,
     pub user: Pubkey,
 }
 
 impl PetAccount {
-    pub const MAX_SIZE: usize = 
+    pub const MAX_SIZE: usize =
         4 + 50 + // nombre
         4 + 50 + // raza
         4 + 50 + // especie
